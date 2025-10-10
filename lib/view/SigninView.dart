c3c75a6892dc9791fbda9930b0f783f478a80a86
jehad630 <devjehad630@gmail.com>
2025-10-10 19:37:56 +0300

    Refactor SigninView to use StatefulWidget; add email and password handling in CustomTextfield

--- lib/view/SigninView.dart
+++ lib/view/SigninView.dart
@@ -3,11 +3,15 @@ import 'package:new_notes_app/view/Notes_view.dart';
 import 'package:new_notes_app/widget/ButtonWidget.dart';
 import 'package:new_notes_app/widget/TextField.dart';
 
-class Signinview extends StatelessWidget {
+class Signinview extends StatefulWidget {
   const Signinview({super.key});
-
   final String id = "Signinview";
+  @override
+  State<Signinview> createState() => _SigninviewState();
+}
 
+class _SigninviewState extends State<Signinview> {
+  String? email, password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,10 +29,22 @@ class Signinview extends StatelessWidget {
             "  Sign in",
             style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
           ),
-          CustomTextfield(hint: "email", obscureText: false),
-          CustomTextfield(hint: "Password", obscureText: true),
+          CustomTextfield(
+            hint: "email",
+            obscureText: false,
+            onChanged: (data) {
+              email = data;
+            },
+          ),
+          CustomTextfield(
+            hint: "Password",
+            obscureText: true,
+            onChanged: (data) {
+              password = data;
+            },
+          ),
           ButtonWidget(
-            color: Color(0xFFD36836),
+            color: Color(0xFF4D5A68),
             text: "Sign in",
             onTap: () {
               Navigator.pushNamed(context, NotesView().id);
--- lib/widget/TextField.dart
+++ lib/widget/TextField.dart
@@ -1,31 +1,39 @@
 import 'package:flutter/material.dart';
 
 class CustomTextfield extends StatelessWidget {
-  const CustomTextfield({
+   CustomTextfield({
     super.key,
     required this.hint,
     required this.obscureText,
+    this.onChanged,
   });
 
   final String hint;
   final bool obscureText;
+  Function(String)? onChanged;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: TextFormField(
+        validator: (data) {
+          if (data!.isEmpty) {
+            return "filed is requierd";
+          }
+        },
+        onChanged: onChanged,
         obscureText: obscureText,
         decoration: InputDecoration(
           hintText: hint,
           hintStyle: TextStyle(fontWeight: FontWeight.bold),
           enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide(color: Color(0xFF83E1FF), width: 3),
+            borderSide: BorderSide(color: Color(0xFFFFCA42), width: 3),
           ),
           focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide(color: Color(0xFFB560FF), width: 3),
+            borderSide: BorderSide(color: Color(0xFFD36836), width: 3),
           ),
           errorBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(12),
@@ -33,7 +41,7 @@ class CustomTextfield extends StatelessWidget {
           ),
           focusedErrorBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide(color: Color(0xFFB560FF), width: 3),
+            borderSide: BorderSide(color: Color(0xFFB12E65), width: 3),
           ),
         ),
       ),
