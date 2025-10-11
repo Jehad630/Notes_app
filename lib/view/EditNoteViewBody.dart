89960deb9e30a2e8bfec1c68dd1cd8491acabd33
jehad630 <devjehad630@gmail.com>
2025-10-11 18:13:00 +0300

    Add EditNoteViewBody and NoteModel: implement note editing UI and data model

--- lib/view/EditNoteViewBody.dart
+++ lib/view/EditNoteViewBody.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:new_notes_app/widget/TextField.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -6,7 +7,16 @@ class EditNoteViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Edit Note"),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.check,size: 32,))
+        ],
+      ),
+      body: Column(children: [
+        CustomTextfield(hint: "title", obscureText: false),
+        CustomTextfield(hint: "content", obscureText: false, maxLines: 5),
+      ],),
     );
   }
 }
